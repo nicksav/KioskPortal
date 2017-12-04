10b621ccd44488775bb098841bf0b42e47d37b1c
nicksav <inci@dezcom.org>
2017-12-04 10:50:39 +1000

    deplyed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor() {
+    window.addEventListener("message", this.recevedTokenMessage, false);
+  }
+
+  public recevedTokenMessage(event){
+    debugger;
+  }
 }
+
+
